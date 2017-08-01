8b08f3095582e065b1c8e0d09c08f62cdf69ffcf
Henry Jao <jaohaohsuan@gmail.com>
2017-08-01 17:03:11 +0800

    add trait to messages

---
+++ protobuf/src/main/scala/com/grandsys/inu/models/Command.scala
@@ -0,0 +1,6 @@
+package com.grandsys.inu.models
+
+/**
+  * Created by henry on 8/1/17.
+  */
+trait Command { }
--- protobuf/src/test/scala/com/grandsys/inu/protos/storedq/StoredQueryGrpcTest.scala
+++ protobuf/src/test/scala/com/grandsys/inu/protos/storedq/StoredQueryGrpcTest.scala
@@ -5,6 +5,7 @@ import org.scalatest._
 class StoredQueryGrpcTest  extends FlatSpec with Matchers  {
 
   "StoredQuery grpc" should "generated" in {
+    val c1: com.grandsys.inu.models.Command = com.grandsys.inu.protos.storedq.commands.CreateStoredQuery()
 
   }
 
