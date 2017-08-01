a8afdd027f82cad2c86c73b39158f167c9be6ea0
Henry Jao <jaohaohsuan@gmail.com>
2017-08-01 17:15:20 +0800

    rewrite version scope

--- build.sbt
+++ build.sbt
@@ -2,10 +2,13 @@ import Dependencies._
 import Settings._
 
 
+version := "0.0.1"
+
 lazy val protobuf = (project in file("protobuf")).
   settings(Settings.settings: _*).
   settings(Settings.protobufSettings: _*).
   settings(
+
     PB.targets in Compile := Seq(
       scalapb.gen() -> (sourceManaged in Compile).value
     ),
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,21 +3,22 @@ import Keys._
 
 object Dependencies {
 
-  lazy val version = new {
+  lazy val libs = new  {
+
+    private val version = new {
       val scalaTest       = "3.0.0"
       val scalaCheck      = "1.13.4"
-  }
+    }
 
-  lazy val libs = new  {
-      val test  = "org.scalatest" %% "scalatest" % version.scalaTest % Test
-      val check = "org.scalacheck" %% "scalacheck" % version.scalaCheck % Test
-      val scalapbRuntime = "com.trueaccord.scalapb" %% "scalapb-runtime" % com.trueaccord.scalapb.compiler.Version.scalapbVersion % "protobuf"
+    val test  = "org.scalatest" %% "scalatest" % version.scalaTest % Test
+    val check = "org.scalacheck" %% "scalacheck" % version.scalaCheck % Test
+    val scalapbRuntime = "com.trueaccord.scalapb" %% "scalapb-runtime" % com.trueaccord.scalapb.compiler.Version.scalapbVersion % "protobuf"
 
-      lazy val grpc = new {
-          import com.trueaccord.scalapb.compiler.Version._
-          val netty = "io.grpc" % "grpc-netty" % grpcJavaVersion
-          val scalapbRuntime = "com.trueaccord.scalapb" %% "scalapb-runtime-grpc" % scalapbVersion
-        }
+    lazy val grpc = new {
+        import com.trueaccord.scalapb.compiler.Version._
+        val netty = "io.grpc" % "grpc-netty" % grpcJavaVersion
+        val scalapbRuntime = "com.trueaccord.scalapb" %% "scalapb-runtime-grpc" % scalapbVersion
+      }
   }
 
 
